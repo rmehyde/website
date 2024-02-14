7cf1814edec49d426846f3f0334c69714b9fc058
Reese Hyde <github@rmehyde.com>
2024-02-13 20:02:26 -0500

    shrink padding for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import MDXCards from "@/app/ui/cards/mdxcards";
 
 export default async function Home() {
     return (
-        <main className="min-h-screen p-14">
+        <main className="min-h-screen p-4 md:p-14">
             <MDXCards contentDir={contentDir}/>
         </main>
     )
